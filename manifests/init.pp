c5462cb4318e5e2945da39843b47bf862355518d
matthew117 <matthew.bates.117@gmail.com>
2021-04-13 00:58:26 +0100

    Added vhost defined type

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,14 @@
 class apache (
   String $install_name,
+  String $service_name,
   Array $config_paths,
+  String $vhost_dir,
 ) {
   contain apache::install
   contain apache::config
+  contain apache::service
 
   Class['apache::install']
   -> Class['apache::config']
+  ~> Class['apache::service']
 }
